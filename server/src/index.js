0c1890813dc6de6547bc1722c66da4c894eb906d
ESCV96 <esthc@outlook.es>
2023-01-07 16:35:34 +0100

    changes in the folders

--- server/src/index.js
+++ server/src/index.js
@@ -23,24 +23,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 // API
-<<<<<<< HEAD:server/src/index.js
 app.get("/api/:songId", (req, res) => {
 	const query = 'SELECT * FROM tickets WHERE iTicketId = ' + req.params.songId
 
-=======
-app.get('/song/:songId', (req, res) => {
-	const query = 'SELECT * FROM tickets WHERE iTicketId = ' + req.params.songId
->>>>>>> 57dc0d33582c04bc337c9b8f7c62e763c6923d09:src/index.js
 	connection.query(query, (err, rows, fields) => {
 		if (err) throw err
 
 		if (rows[0].bIsUsed) {
-<<<<<<< HEAD:server/src/index.js
 			res.json('Acceso denegado');
-=======
-			res.send('Acceso aprobado');
-			res.redirect('/about');
->>>>>>> 57dc0d33582c04bc337c9b8f7c62e763c6923d09:src/index.js
 		} else {
 			res.json("Acceso aprobado");
 		}
